14b5f84a21b1e8536dcf914994b37c62d6bd894f
Nolwenn <you@example.com>
2024-03-29 23:22:36 +0100

    burger menu

--- assets/scripts/nwmDevTool/Tool/Tool.js
+++ assets/scripts/nwmDevTool/Tool/Tool.js
@@ -7,8 +7,6 @@ import Overlay from "../OverlayTool/OverlayTool.js";
  */
 export default class Tool extends HTMLElement
 {
-    // static dragoverClass = "nwm-drag-over";
-    // static dragZoneClass = "nwm-drag-zone";
     static toolClass = "nwm-tool";
     #href = "./assets/scripts/nwmDevTool/"
     lang = "fr";
@@ -42,7 +40,6 @@ export default class Tool extends HTMLElement
         this.classList.add("open", this.constructor.toolClass)
         this.addEventListener("pointerdown", this.#activeTool.bind(this));
         this.#activeTool();
-        // this.draggable = false;
         this.#generateID();
 
         this.container = document.createElement("div");
@@ -53,7 +50,6 @@ export default class Tool extends HTMLElement
         header.addEventListener("pointerdown", this.#startMoveTool.bind(this));
 
         this.header = header;
-        // this.addEventListener("dragstart", this.#dragStart);
         const btnsHeader = document.createElement("div")
         btnsHeader.classList.add("btns-container")
 
@@ -302,89 +298,3 @@ export default class Tool extends HTMLElement
         this.style.height = Math.ceil(cHeight+hHeight)+"px";
     }
 }
-// /**
-//  * Active drag and drop
-//  */
-// #DragAndDropOn()
-// {
-//     this.draggable = true;
-//     window.ondragend = this.#DragAndDropOff.bind(this);
-//     window.onpointerup = this.#DragAndDropOff.bind(this);
-// }
-// /**
-//  * desactive the drag and drop.
-//  */
-// #DragAndDropOff()
-// {
-//     window.ondragend = "";
-//     window.onpointerup = ";"
-//     this.style.opacity = "";
-//     this.draggable = false;
-// }
-// /**
-//  * prepare the tool for a drag and drop.
-//  * @param {DragEvent} e DragEvent gave by a dragStart
-//  */
-// #dragStart(e)
-// {  
-//     this.style.opacity = '0.8';
-//     e.dataTransfer.effectAllowed = 'move';
-//     e.dataTransfer.setData("text/plain", this.id);
-// }
-
-    // /**
-    //  * set Event Listener on HTMLElement for the drag and drop.
-    //  * @param {HTMLElement} dropZone HTMLElement where the tools can be dropped
-    //  */
-    // static setDropZone(dropZone)
-    // {
-    //     dropZone.classList.add(this.dragZoneClass);
-    //     dropZone.addEventListener("dragover", (e)=>e.preventDefault());
-    //     dropZone.addEventListener("dragenter", this.onDragEnter.bind(this));
-    //     dropZone.addEventListener("dragleave", ()=>dropZone.classList.remove(this.dragoverClass));
-    //     dropZone.addEventListener("drop", (e)=>this.onDrop(dropZone, e));
-    // }
-    // /**
-    //  * Move the dragged tool to an empty column or before or after an other tool.
-    //  * @param {HTMLElement} dropZone HTMLElement where the element can be dropped
-    //  * @param {DragEvent} dropEvent DragEvent gave by a drop.
-    //  */
-    // static onDrop(dropZone, dropEvent)
-    // {
-
-    //     const {clientY, target} = dropEvent;
-    //     const data = dropEvent.dataTransfer.getData("text");
-    //     const src = document.querySelector("#"+data);
-    //     console.log(src, target);
-    //     if(src && src != target)
-    //     {
-    //         if(target.classList.contains(this.dragZoneClass))
-    //         {
-    //             dropZone.append(src);
-    //         }
-    //         else if(target.classList.contains(this.toolClass))
-    //         {
-    //             const {height, top} = target.getBoundingClientRect();
-    //             if(top+height/2 > clientY)target.before(src);
-    //             else target.after(src);
-    //         }
-    //     }
-    //     dropZone.classList.remove(this.dragoverClass);
-    // }
-    // /**
-    //  * Add a class when we drag a tool over a dragzone
-    //  * @param {DragEvent} e DragEvent gave by a dragenter
-    //  */
-    // static onDragEnter(e)
-    // {
-    //     let target;
-    //     if(e.target.classList.contains(this.dragZoneClass))
-    //     {
-    //         target = e.target
-    //     }
-    //     else
-    //     {
-    //         target = e.target.closest('.'+this.dragZoneClass);
-    //     }
-    //     target.classList.add(this.dragoverClass);
-    // }
--- assets/scripts/nwmDevTool/index.js
+++ assets/scripts/nwmDevTool/index.js
@@ -3,15 +3,9 @@ import GridTool from "./GridTool/GridTool.js";
 import BurgerMenu from "../BurgerMenu/BurgerMenu.js";
 
 const tools = [GridTool, BoxShadowTool];
-// const columns = document.querySelectorAll(".column");
-// const btnTools = document.querySelector(".btn-tools");
-// const navTools = document.querySelector(".tools-header nav");
 const container = document.querySelector(".tools-container");
 
 generateMenu();
-// columns.forEach((col)=>GridTool.setDropZone(col));
-// btnTools.addEventListener("pointerup", toggleMenu);
-// navTools.addEventListener("pointerup", toggleMenu);
 
 function generateMenu()
 {
@@ -32,12 +26,5 @@ function generateMenu()
 }
 function appendTool()
 {
-    // const column = [...columns].reduce(getSmallestColumn)
-    // if(!column)return;
     container.append(new this());
 }
-// function getSmallestColumn(a,b)
-// {
-//     if(a.children.length<b.children.length) return a;
-//     else return b; 
-// }
